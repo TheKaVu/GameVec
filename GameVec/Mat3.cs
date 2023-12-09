fcab01a5f8e42a6c5612c708038fd479d131e9bd
TheKaVu <k.wieczaszek@gmail.com>
2023-12-09 20:33:05 +0100

    Add missing Mat3 documentation.

--- GameVec/Mat3.cs
+++ GameVec/Mat3.cs
@@ -2,12 +2,22 @@
 
 namespace WinXPP.GameVec
 {
+    /// <summary>
+    /// Represents a 3x3 matrix.
+    /// </summary>
     public struct Mat3
     {
+        /// <summary>
+        /// Size of 3x3 matrix.
+        /// </summary>
         public static readonly int SIZE = 3;
 
         private double[] _mat;
 
+        /// <summary>
+        /// Gets the determinant of this matrix.
+        /// </summary>
+        /// <returns>The determinant of this matrix.</returns>
         public double Det
         {
             get
@@ -22,6 +32,12 @@ namespace WinXPP.GameVec
             }
         }
 
+        /// <summary>
+        /// Creates new <see cref="Mat3"/> structure using an array. Matrix cells are assigned from top to bottom, left to right.<para/>
+        /// <b>Note: </b> Array must be of size 9.
+        /// </summary>
+        /// <param name="mat">Array containing cells of new matrix.</param>
+        /// <exception cref="InvalidMatrixArrayException">Thrown when designated array isn't of size 9.</exception>
         public Mat3(double[] mat)
         {
             int s = SIZE * SIZE;
@@ -31,6 +47,9 @@ namespace WinXPP.GameVec
             mat.CopyTo(_mat, 0);
         }
 
+        /// <summary>
+        /// Creates new <see cref="Mat3"/> structure with cells of specified values.
+        /// </summary>
         public Mat3(
             double x1, double x2, double x3,
             double y1, double y2, double y3,
@@ -42,11 +61,24 @@ namespace WinXPP.GameVec
                 z1, z2, z3
             }) { }
 
+        /// <summary>
+        /// Gets or sets the value of this matrix's cell in specified location.
+        /// </summary>
+        /// <param name="r">Row (left to right)</param>
+        /// <param name="c">Column (left to right)</param>
+        /// <returns>The value of this matrix cell in specified location.</returns>
         public double this[int r, int c]
         {
             get { return _mat[r * SIZE + c]; }
             set { _mat[r * SIZE + c] = value; }
         }
+
+        /// <summary>
+        /// Multiplies following matrix by scalar.
+        /// </summary>
+        /// <param name="mat">Matrix to be multiplied.</param>
+        /// <param name="d">Scalar.</param>
+        /// <returns>Multiplied matrix.</returns>
         public static Mat3 operator *(Mat3 mat, double d)
         {
             for (int i = 0; i < mat._mat.Length; i++)
@@ -55,6 +87,13 @@ namespace WinXPP.GameVec
             }
             return mat;
         }
+
+        /// <summary>
+        /// Multiplies two following matrices.
+        /// </summary>
+        /// <param name="m1">First matrix.</param>
+        /// <param name="m2">Second matrix.</param>
+        /// <returns>Dot product of two matrices.</returns>
         public static Mat3 operator *(Mat3 m1, Mat3 m2)
         {
             int s = SIZE * SIZE;
