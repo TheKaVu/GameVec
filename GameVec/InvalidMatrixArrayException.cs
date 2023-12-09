b805b1bf4986d7ccc92e0a375cc8112a8c718874
TheKaVu <k.wieczaszek@gmail.com>
2023-12-09 15:32:56 +0100

    Add missing InvalidMatrixArrayException documentation.

--- GameVec/InvalidMatrixArrayException.cs
+++ GameVec/InvalidMatrixArrayException.cs
@@ -2,8 +2,14 @@
 
 namespace WinXPP.GameVec
 {
+    /// <summary>
+    /// Represents error that occurs during an attempt of creating matrix out of invalid array.
+    /// </summary>
     internal class InvalidMatrixArrayException : Exception
     {
+        /// <summary>
+        /// Initializes new instance of <see cref="InvalidMatrixArrayException"/> class.
+        /// </summary>
         public InvalidMatrixArrayException() : base("Tried to create matrix out of array of unfitting size.") { }
     }
 }
