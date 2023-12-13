ca70466c9ebf7b417c3ea01784d0c1280d7e9d05
TheKaVu <k.wieczaszek@gmail.com>
2023-12-13 20:10:10 +0100

    Add EntityType class. (#1)

---
+++ GameVec/Simulated/EntityType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinXPP.GameVec.Simulated
+{
+    public class EntityType<T> : EntityType where T : Entity, new()
+    {
+        internal EntityType(string typeId/*, ModelAssembler modelAssembler*/) : base(typeId/*, modelAssembler*/) { }
+
+        internal T Create()
+        {
+            return new T();
+        }
+    }
+
+    public class EntityType
+    {
+        private static readonly Dictionary<Type, EntityType> _registered = new Dictionary<Type, EntityType>();
+        
+        public static EntityType<T> Register<T>(string typeId) where T : Entity, new() 
+        {
+            if(_registered.ContainsKey(typeof(T))) throw new EntityTypeDoubleRegistrationException();
+
+            EntityType<T> entityType = new EntityType<T>(typeId);
+            _registered.Add(typeof(T), entityType);
+            return entityType;
+        }
+
+        /*private readonly ModelAssembler _modelAssembler;*/
+        public string TypeId { get; }
+
+        public EntityType(string typeId /*ModelAssembler modelAssembler*/)
+        {
+            TypeId = typeId;
+            /*_modelAssembler = modelAssembler;*/
+        }
+
+        /*public IModel GetNewModel()
+        {
+            return _modelAssembler.Assemble();
+        }*/
+    }
+}
