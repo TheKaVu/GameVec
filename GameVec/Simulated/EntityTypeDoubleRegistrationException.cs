d7cb4de752b5b2334e307e000a6caafb8564c200
TheKaVu <k.wieczaszek@gmail.com>
2023-12-13 20:02:51 +0100

    Add EntityTypeDoubleRegistrationException class. (#1)

---
+++ GameVec/Simulated/EntityTypeDoubleRegistrationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinXPP.GameVec.Simulated
+{
+    internal class EntityTypeDoubleRegistrationException : Exception
+    {
+        public EntityTypeDoubleRegistrationException() : base("Tried to register same entity type twice.") { }
+    }
+}
