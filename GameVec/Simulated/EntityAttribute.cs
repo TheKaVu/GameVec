facecc79dba57d7408d87cf96fdeb1865cd21b77
TheKaVu <k.wieczaszek@gmail.com>
2023-12-13 19:56:19 +0100

    Add EntityAttribue struct. (#1)

---
+++ GameVec/Simulated/EntityAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WinXPP.GameVec.Simulated
+{
+    public readonly struct EntityAttribute
+    {
+        public string Name { get; }
+
+        public EntityAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public static bool operator ==(EntityAttribute left, EntityAttribute right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(EntityAttribute left, EntityAttribute right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EntityAttribute attribute &&
+                   Name.Equals(attribute.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1969571243 + Name.GetHashCode();
+        }
+    }
+}
