a30205f3db0a524c6d00cd4c5f0779a21bc6fdab
Mitons <64582096+Mitons@users.noreply.github.com>
2023-12-28 15:07:38 +0100

    Add MeshModel class. (#12)

---
+++ GameVec/Render/MeshModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace WinXPP.GameVec.Render
+{
+    public class MeshModel : IModel
+    {
+        private readonly HashSet<IModel> _subModels;
+        private readonly HashSet<Vertice> _vertices;
+
+        public Color Color { get;}
+        public int Thickness { get; }
+        public Vec3 Center { get; }
+        public Vec3 Rotation { get; set; }
+        public HashSet<IModel> SubModels => new HashSet<IModel>(_subModels);
+        public HashSet<Vertice> Vertices => new HashSet<Vertice>(_vertices);
+
+        public MeshModel(HashSet<IModel> subModels, HashSet<Vertice> vertices, Color color, int thickness, Vec3 center, Vec3 rotation)
+        {
+            _subModels = new HashSet<IModel>(subModels);
+            _vertices = new HashSet<Vertice>(vertices);
+            Color = color;
+            Thickness = thickness;
+            Center = center;
+            Rotation = rotation;
+        }
+    }
+}
