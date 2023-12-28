52e84c555efa6077b542487582b21790245df49d
Mitons <64582096+Mitons@users.noreply.github.com>
2023-12-28 15:07:24 +0100

    Add IModel interface. (#12)

---
+++ GameVec/Render/IModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic; 
+
+namespace WinXPP.GameVec.Render
+{
+    public interface IModel
+    {
+        Vec3 Center { get;}
+        Vec3 Rotation { get; set; }
+        HashSet<IModel> SubModels { get;}
+        HashSet<Vertice> Vertices { get;}
+    }
+}
