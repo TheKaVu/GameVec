afeed070248264069fdf77c98b0ad4a08505270e
Marcin Chwastek <kalkulator1123456@gmail.com>
2024-01-21 12:19:28 +0100

    Add WordGrid class. (#5)

---
+++ GameVec/Simulated/WordGrid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WinXPP.GameVec.Simulated
+{
+	public class WorldGrid
+	{
+		private readonly HashSet<Entity> _entities = new HashSet<Entity>();
+
+		public HashSet<Entity> Entities => new HashSet<Entity>(_entities);
+
+        public void SpawnEntity<T> (EntityType<T> entityType) where T : Entity, new()
+		{ 
+			_entities.Add(entityType.Create());
+		}
+
+		public bool Exists(Entity entity)
+		{
+			return _entities.Contains(entity);
+		}
+
+		public void DeleteEntity(Entity entity)
+		{
+			_entities.Remove(entity);
+		}
+
+		public Entity GetEntity(Guid uuid)
+		{
+			foreach(var entity in _entities)
+			{
+				if (entity.UUID == uuid) return entity;
+			}
+		}
+    }
+}
